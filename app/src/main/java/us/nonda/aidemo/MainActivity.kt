5b756775fe56cdd986d3fc6b2a99c24243ef1ce1
Nonda-He <he@nonda.us>
2019-01-14 14:40:23 +0800

    feat: add pocketsphinx aar

--- app/src/main/java/us/nonda/aidemo/MainActivity.kt
+++ app/src/main/java/us/nonda/aidemo/MainActivity.kt
@@ -1,7 +1,7 @@
 package us.nonda.aidemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ models/build/generated/source/buildConfig/debug/us/nonda/models/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package us.nonda.models;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "us.nonda.models";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
